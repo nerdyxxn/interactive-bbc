e0f40971647ccfbc2243ab29d0be5fe861bc895d
윤성 <yunyul1209@gmail.com>
2023-08-06 15:44:29 +0900

    feat : ioIndex를 이용해 현재 눈에 보이는 요소와 직전 & 직후만 for문 수행

--- main.js
+++ main.js
@@ -3,9 +3,19 @@
   const graphicElems = document.querySelectorAll('.graphic-item');
   // 현재 활성화된(visible 클래스가 붙은) .graphic-item을 지정
   let currentItem = graphicElems[0];
+  let ioIndex;
+
+  /* Intersection Observer를 이용해서 해당 요소가 눈에 보이는지 체크
+     IntersectionObserver의 관찰 대상인 객체들이 사라지거나 나타날 때, 그 시점마다 콜백함수가 실행된다. */
+  const io = new IntersectionObserver((entries, observer) => {
+    ioIndex = entries[0].target.dataset.index * 1;
+  });
 
   for (let i = 0; i < stepElems.length; i++) {
-    //stepElemsp[i].setAttribute('data-index', i);
+    // 모든 stepElems를 관찰 대상으로 등록
+    io.observe(stepElems[i]);
+
+    // stepElemsp[i].setAttribute('data-index', i);
     stepElems[i].dataset.index = i;
     graphicElems[i].dataset.index = i;
   }
@@ -24,11 +34,16 @@
     // stepElems 말풍선들의 위치 체크해서 이벤트 핸들링 (위치 범위 안에 들어왔을 때 활성화)
     let step;
     let boundingRect;
+    let temp = 0;
 
-    for (let i = 0; i < stepElems.length; i++) {
+    // ioIndex를 이용해 현재 눈에 보이는 요소와 직전 & 직후만 for문 돌도록 수정
+    // for (let i = 0; i < stepElems.length; i++) {
+    for (let i = ioIndex - 1; i < ioIndex + 2; i++) {
       step = stepElems[i];
+      if (!step) continue;
       boundingRect = step.getBoundingClientRect();
-      //console.log(boundingRect.top);
+
+      temp++;
 
       if (
         boundingRect.top > window.innerHeight * 0.1 &&
@@ -39,6 +54,8 @@
         activate();
       }
     }
+    console.log(temp);
   });
+
   activate();
 })();
