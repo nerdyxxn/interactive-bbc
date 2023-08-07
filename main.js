b966c88476ba8b3b28c2741aef6662e53569c973
윤성 <yunyul1209@gmail.com>
2023-08-07 15:07:26 +0900

    feat : birdFlies2() 두 번째 새 애니메이션 추가
    - 화면 로드 시 스크롤 최상단으로 이동하도록 수정

--- main.js
+++ main.js
@@ -10,6 +10,16 @@
         document.querySelector('[data-index="2"] .bird').style.transform = `translateX(-100%)`;
       }
     },
+
+    birdFlies2(key) {
+      if (key) {
+        document.querySelector('[data-index="5"] .bird').style.transform = `translate(${
+          window.innerWidth
+        }px, ${-window.innerHeight * 0.7}px)`;
+      } else {
+        document.querySelector('[data-index="5"] .bird').style.transform = `translateX(-100%)`;
+      }
+    },
   };
 
   const stepElems = document.querySelectorAll('.step');
@@ -74,6 +84,10 @@
       }
     }
     //console.log(temp);
+  });
+
+  window.addEventListener('load', () => {
+    setTimeout(() => scrollTo(0, 0), 100);
   });
 
   activate();
